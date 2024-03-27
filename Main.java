18984f7a4ed181dc1b90fcf525fb12d859d8ea26
Andrzej Sedzik <39155518+andrzej-sedzik-dev@users.noreply.github.com>
2024-03-27 08:09:58 +0100

    commit-7

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello 2!");
         System.out.println("Hello 3!");
         System.out.println("Hello 4!");
+        System.out.println("Hello 5!");
     }
 }
